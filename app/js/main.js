e35803bbe1eec82bca833df45ee5bad50c1651a9
Frudee <dsukhorukov16@gmail.com>
2021-12-21 15:06:04 +0300

    final adj

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,21 @@
-const container = document.querySelector(".list");
-let q = 5;
-const getRandomIntInclusive = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-const data = fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((response) => response.json())
-  .catch((err) => alert(err))
-  .then((res) => {
-    for (let i = 0; i < q; i++) {
-      let random = getRandomIntInclusive(0, res.length);
-      let randomDouble = getRandomIntInclusive(0, res.length);
-      let descChance = getRandomIntInclusive(1, 4);
-      //   console.log(res);
-      container.insertAdjacentHTML(
-        "beforeend",
-        `<li>
-        <div class="card__item">
+const getData = (q = 5) => {
+  const container = document.querySelector(".list");
+  const getRandomIntInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((response) => response.json())
+    .catch((err) => console.log(err))
+    .then((res) => {
+      for (let i = 0; i < q; i++) {
+        let random = getRandomIntInclusive(0, res.length - 1);
+        let randomDouble = getRandomIntInclusive(0, res.length - 1);
+        let descChance = getRandomIntInclusive(1, 4);
+        container.insertAdjacentHTML(
+          "beforeend",
+          `<li>       
           <label for=${res[random].id}
             >           
             <input type="checkbox" id=${res[random].id} />
@@ -27,12 +24,10 @@ const data = fetch("https://jsonplaceholder.typicode.com/todos")
             ${
               descChance === 1 ? `<span>${res[randomDouble].title}</span>` : ""
             }          
-          </label>
-        </div>
+          </label>        
       </li>`
-      );
-    }
-  });
-//   создать функцию с агрументом количества и колвом по дефолту, перенести все в эту функ
-// ловить ошибки
-// в мобильной версии сразу зачеркнуто
+        );
+      }
+    });
+};
+getData();
